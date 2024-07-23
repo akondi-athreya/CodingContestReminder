7d8a6372e1c3bad843e09eddcebd1196516d4296
akondi-athreya <akondiathreya@gmail.com>
2024-07-23 22:06:02 +0530

    day 2 by athreya

---
+++ js/gfg_data.js
@@ -0,0 +1,53 @@
+var gfg = [
+  {
+    "Title": "Job-A-Thon 35 Hiring Challenge",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/job-a-thon-35-hiring-challenge",
+    "Like": "https://www.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Fjob-a-thon-35-hiring-challenge-1719993235-desktop.png&w=828&q=75",
+    "sofiapro": "July 22, 2024",
+    "City": "08:00 PM IST"
+  },
+  {
+    "Title": "GFG Weekly - 165 [Rated Contest]",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/gfg-weekly-165-rated-contest",
+    "Like": "https://www.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Fgfg-weekly-165-rated-contest-1721373307-desktop.png&w=828&q=75",
+    "sofiapro": "July 28, 2024",
+    "City": "07:00 PM IST"
+  },
+  {
+    "Title": "GFG Weekly - 166 [Rated Contest]",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/gfg-weekly-166-rated-contest",
+    "Like": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    "sofiapro": "August 04, 2024",
+    "City": "07:00 PM IST"
+  },
+  {
+    "Title": "GFG Weekly - 167 [Rated Contest]",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/gfg-weekly-167-rated-contest",
+    "Like": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    "sofiapro": "August 11, 2024",
+    "City": "07:00 PM IST"
+  },
+  {
+    "Title": "GFG Weekly - 164 [Rated Contest]",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/gfg-weekly-164-rated-contest",
+    "Like": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    "sofiapro": "July 21, 2024",
+    "City": "07:00 PM IST"
+  },
+  {
+    "Title": "GFG Weekly - 163 [Rated Contest]",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/gfg-weekly-163-rated-contest",
+    "Like": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    "sofiapro": "July 14, 2024",
+    "City": "07:00 PM IST"
+  },
+  {
+    "Title": "GFG Weekly - 162 [Rated Contest]",
+    "Title_URL": "https://practice.geeksforgeeks.org/contest/gfg-weekly-162-rated-contest",
+    "Like": "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+    "sofiapro": "July 07, 2024",
+    "City": "07:00 PM IST"
+  }
+]
+
+// export default gfg;
