18ba30afd4d7711132dfa4d7f859df4dbcdf4fb9
Alekhya <alekhyanam@gmail.com>
2024-07-27 09:26:32 +0530

    SignUpPage

---
+++ js/js2.js
@@ -0,0 +1,34 @@
+var userNameDiv;
+function userNameLogin(value){
+    userNameDiv=value;
+}
+var passwdUserDiv;
+function passwdLogin(value){
+    passwdUserDiv=value;
+}
+var usernameLoginDivErase=document.getElementsByClassName("loginTextFiledOne")[0];
+var passwdLoginDivErase=document.getElementsByClassName("loginTextFiledTwo")[0];
+function loginButton(formname) {
+        var Username = (userNameDiv);
+        var Password = (passwdUserDiv);
+        console.log("Entered Username:", Username);
+    console.log("Entered Password:", Password);
+        const usersString = localStorage.getItem('users');
+        const usersArray = JSON.parse(usersString);
+        const user = usersArray.find(user => user.username === Username && user.password === Password);
+    
+        if (user) {
+            console.log("hai")
+            // alert('Login successful');
+            location.href="../SignUpPages.html";
+            passwdLoginDivErase.reset();
+        } else {
+            alert("login failed");
+            var form = document.getElementsByClassName(formname)[0];
+            console.log(form);
+            form.reset();
+            // usernameLoginDivErase.innerHTML="";
+            // console.log(usernameLoginDivErase.target.value)
+
+        }
+}
---
+++ localstorage.js
@@ -0,0 +1,21 @@
+// let users = [
+//     {
+//         'username':'gangadhar',
+//         'usermail':'saigangadharsgk@gmail.com',
+//         'password':'123',
+//         'gfg':'gangadhar',
+//         'leetcode':'gangadhar',
+//         'codechef':'gangadhar',
+//         'codeforces':'gangadhar'
+//     }
+// ]
+
+// localStorage.setItem('users', JSON.stringify(users));
+
+// function GetUsers() {
+//     const usersString = localStorage.getItem('users');
+//     const usersArray = JSON.parse(usersString);
+//     console.log(usersArray);
+// }
+
+// GetUsers();
